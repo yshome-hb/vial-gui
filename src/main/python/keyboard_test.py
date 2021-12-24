7c20f198c3a44698e51c8967911a82b5376e81cc
ysshen <hustshenyang@126.com>
2021-12-24 17:51:17 +0800

    feat：精简keyboard_test的代码

--- src/main/python/keyboard_test.py
+++ src/main/python/keyboard_test.py
@@ -5,10 +5,9 @@ from PyQt5.QtCore import Qt, QTimer
 import math
 
 from basic_editor import BasicEditor
-from keyboard_widget import KeyboardWidget
 from util import tr
 from vial_device import VialKeyboard
-from unlocker import Unlocker
+from key_widget import KeyWidget
 
 
 class KeyboardTest(BasicEditor):
@@ -18,16 +17,7 @@ class KeyboardTest(BasicEditor):
 
         self.layout_editor = layout_editor
 
-        self.keyboardWidget = KeyboardWidget(layout_editor)
-        self.keyboardWidget.set_enabled(False)
-
         self.reset_btn = QPushButton("Reset")
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.keyboardWidget)
-        layout.setAlignment(self.keyboardWidget, Qt.AlignCenter)
-
-        self.addLayout(layout)
 
         btn_layout = QHBoxLayout()
         btn_layout.addStretch()
@@ -36,12 +26,10 @@ class KeyboardTest(BasicEditor):
 
         self.keyboard = None
         self.device = None
-        self.polling = False
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.matrix_poller)
+        self.keyboard_ui()
 
-        self.reset_btn.clicked.connect(self.reset_keyboard_widget)
+        self.reset_btn.clicked.connect(self.reset_keyboard_ui)
 
         self.grabber = QWidget()
 
@@ -50,14 +38,11 @@ class KeyboardTest(BasicEditor):
         if self.valid():
             self.keyboard = device.keyboard
 
-            self.keyboardWidget.set_keys(self.keyboard.keys, self.keyboard.encoders)
-        self.keyboardWidget.setEnabled(self.valid())
-
     def valid(self):
         # Check if vial protocol is v3 or later
         return isinstance(self.device, VialKeyboard)
 
-    def reset_keyboard_widget(self):
+    def reset_keyboard_ui(self):
         # reset keyboard widget
         for w in self.keyboardWidget.widgets:
             w.setPressed(False)
@@ -67,68 +52,14 @@ class KeyboardTest(BasicEditor):
         self.keyboardWidget.update()
         self.keyboardWidget.updateGeometry()
 
-    def matrix_poller(self):
-        if not self.valid():
-            self.timer.stop()
-            return
-
-        try:
-            unlocked = self.keyboard.get_unlock_status(3)
-        except (RuntimeError, ValueError):
-            self.timer.stop()
-            return
-
-        # Get size for matrix
-        rows = self.keyboard.rows
-        cols = self.keyboard.cols
-        # Generate 2d array of matrix
-        matrix = [[None] * cols for x in range(rows)]
-
-        # Get matrix data from keyboard
-        try:
-            data = self.keyboard.matrix_poll()
-        except (RuntimeError, ValueError):
-            self.timer.stop()
-            return
-
-        # Calculate the amount of bytes belong to 1 row, each bit is 1 key, so per 8 keys in a row,
-        # a byte is needed for the row.
-        row_size = math.ceil(cols / 8)
-
-        for row in range(rows):
-            # Make slice of bytes for the row (skip first 2 bytes, they're for VIAL)
-            row_data_start = 2 + (row * row_size)
-            row_data_end = row_data_start + row_size
-            row_data = data[row_data_start:row_data_end]
-
-            # Get each bit representing pressed state for col
-            for col in range(cols):
-                # row_data is array of bytes, calculate in which byte the col is located
-                col_byte = len(row_data) - 1 - math.floor(col / 8)
-                # since we select a single byte as slice of byte, mod 8 to get nth pos of byte
-                col_mod = (col % 8)
-                # write to matrix array
-                matrix[row][col] = (row_data[col_byte] >> col_mod) & 1
-
-        # write matrix state to keyboard widget
-        for w in self.keyboardWidget.widgets:
-            if w.desc.row is not None and w.desc.col is not None:
-                row = w.desc.row
-                col = w.desc.col
-
-                if row < len(matrix) and col < len(matrix[row]):
-                    w.setPressed(matrix[row][col])
-                    if matrix[row][col]:
-                        w.setActive(True)
-
-        self.keyboardWidget.update_layout()
-        self.keyboardWidget.update()
-        self.keyboardWidget.updateGeometry()
+    def keyboard_ui(self):
+        layout = QVBoxLayout()
+        layout.addWidget(KeyWidget())
+        # layout.setAlignment(self.keyboardWidget, Qt.AlignCenter)
+        self.addLayout(layout)
 
     def activate(self):
         self.grabber.grabKeyboard()
-        self.timer.start(20)
 
     def deactivate(self):
         self.grabber.releaseKeyboard()
-        self.timer.stop()
